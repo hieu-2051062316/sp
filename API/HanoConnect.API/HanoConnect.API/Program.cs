a4d2a0f56404548631880ee4695eb409780050f4
veainz <bdluong.hubnetwork@gmail.com>
2025-06-19 14:45:10 +0700

    ConnectToTheDatabase

--- API/HanoConnect.API/HanoConnect.API/Program.cs
+++ API/HanoConnect.API/HanoConnect.API/Program.cs
@@ -1,3 +1,6 @@
+﻿using HanoConnect.API.Data; // Thêm dòng này để sử dụng ApplicationDbContext
+using Microsoft.EntityFrameworkCore; // Thêm dòng này để sử dụng các phương thức mở rộng của EF Core
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// --- Bắt đầu phần thêm code của bạn ---
+
+// Đăng ký ApplicationDbContext với SQL Server và chuỗi kết nối từ appsettings.Development.json
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// --- Kết thúc phần thêm code của bạn ---
 
 var app = builder.Build();
 

bb6c5c935b7e3263c0db7b7f15e1b143d92039b5
veainz <bdluong.hubnetwork@gmail.com>
2025-06-25 00:12:06 +0700

    Create SQLQueryDropNotification.sql

---
+++ SQL/SQLQueryDropNotification.sql
@@ -0,0 +1,15 @@
+﻿USE HanoConnectDB;
+GO
+
+-- Xóa khóa ngoại trỏ đến bảng Notifications nếu có
+-- (Trong trường hợp này không có, nhưng đây là bước phòng ngừa tốt)
+-- ALTER TABLE ... DROP CONSTRAINT ...;
+
+-- Xóa bảng Notifications
+IF OBJECT_ID('dbo.Notifications', 'U') IS NOT NULL
+    DROP TABLE dbo.Notifications;
+GO
+
+-- Xóa bản ghi migration liên quan để tránh lỗi sau này
+DELETE FROM [__EFMigrationsHistory] WHERE MigrationId LIKE '%AddNotificationsTable%';
+GO
